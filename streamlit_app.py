f4725e91ee498d606da5f0aa7737c3a26bf4de8f
Aryan-401 <aryan.raj.garg@gmail.com>
2024-03-05 11:51:42 +0530

    streamlit init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,73 @@
 import streamlit as st
 import requests
+import io
+from PIL import Image
 import base64
 
 st.title("Food Classification API")
 st.write("This is a simple API that classifies food images into 20 classes")
 st.write("The food can be classified on the following classes:")
-st.write(
-    """
-    - Burger
-    - Butter Naan
-    - Chai
-    - Chapati
-    - Chole Bature
-    - Dal Maakhani
-    - Dhokla
-    - Fried Rice
-    - Idli
-    - Jalebi
-    - Kaathi Rolls
-    - Kadai Paneer
-    - Kulfi
-    - Masala Dosa
-    - Momos
-    - Paani Puri
-    - Pakode
-    - Pav Bhaji
-    - Pizza
-    - Samosa
-    """
+st.table(
+    {
+        " ": [
+            "Burger",
+            "Butter Naan",
+            "Chai",
+            "Chapati",
+            "Chole Bature",
+            ],
+        "  ": [
+            "Dal Maakhani",
+            "Dhokla",
+            "Fried Rice",
+            "Idli",
+            "Jalebi",
+            ],
+        "   ": [
+            "Kaathi Rolls",
+            "Kadai Paneer",
+            "Kulfi",
+            "Masala Dosa",
+            "Momos",
+            ],
+        "    ": [
+            "Paani Puri",
+            "Pakode",
+            "Pav Bhaji",
+            "Pizza",
+            "Samosa",
+        ]
+    }
 )
 st.write("Upload an image of food to classify")
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    with open(uploaded_file.name, "rb") as f:
-        image = f.read()
-        image_base64 = base64.b64encode(image).decode("utf-8")
-    response = requests.post(r'https://calorie-click-api.onrender.com/predict', json=image_base64)
-    st.json(response.json())
+    input_image = Image.open(uploaded_file)
+    file_extension = uploaded_file.name.split(".")[-1].upper()
+    if file_extension == "JPG":
+        file_extension = "JPEG"
+    buffered = io.BytesIO()
+    input_image.save(buffered, format=file_extension)
+    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    print(img_str)
+    with st.spinner("This may take a while depending if the model is loaded or not. Please be patient."):
+        response = requests.get(
+            url="https://calorie-click-api.onrender.com")
+        if response.status_code == 200:
+            st.success("Model is loaded")
+
+        response = requests.post(
+            url="https://calorie-click-api.onrender.com/predict",
+            json={"base64_image": img_str},
+        )
+        if response.status_code == 200:
+            st.success("Image Classified")
+            top_classes = response.json().get("top_classes")
+            st.table(
+                {
+                    "Class": top_classes,
+                }
+            )
+        else:
+            st.error("Error occurred while classifying the image")
